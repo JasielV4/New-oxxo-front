951dbd12b0c4ae98d2f66926deeaad4c5aae5f1e
JasielV4 <bioplayer1234@gmail.com>
2024-10-26 21:01:18 -0600

    feat: Modal para editar location

--- actions/locations/create.ts
+++ actions/locations/create.ts
@@ -1,7 +1,9 @@
 "use server";
 import { API_URL } from "@/constants";
+import { Location } from "@/entities";
 import { authHeaders } from "@/helpers/authHeaders";
 import { revalidatePath, revalidateTag } from "next/cache";
+import { redirect } from "next/navigation";
 
 export async function createLocation(formData: FormData): Promise<void> {
     let location: any = {}
@@ -23,8 +25,13 @@ export async function createLocation(formData: FormData): Promise<void> {
         method: "POST",
         body: JSON.stringify(location),
         headers: {
+            'content-type': 'application/json',
             ...authHeaders(),
         }
     })
-    if (response.status === 201) revalidateTag("dashboard:locations");
+    const { locationId }: Location = await response.json()
+    if (response.status === 201) {
+        revalidateTag("dashboard:locations");
+        redirect(`/dashboard?store=${locationId}`)
+    }
 }
--- app/dashboard/@locations/_components/SelectManager.tsx
+++ app/dashboard/@locations/_components/SelectManager.tsx
@@ -2,12 +2,17 @@
 import { Select, SelectItem } from "@nextui-org/react";
 import { Location, Manager } from "@/entities";
 
-export default function SelectManager({ managers, locations }: { managers: Manager[], locations: Location[] }) {
+interface SelectManagerProps {
+    managers: Manager[],
+    locations: Location[],
+    defaultManager?: string,
+}
+export default function SelectManager({ managers, locations, defaultManager }: SelectManagerProps) {
     const disabledKeys = locations.map((location: Location) => {
         return location.manager?.managerId
     }).filter((managerId) => managerId != undefined)
     return (
-        <Select label="Manager" name="manager">
+        <Select defaultSelectedKeys={defaultManager != undefined ? [defaultManager] : []} label="Manager" name="manager">
             {managers.map((manager: Manager) => {
                 return (
                     <SelectItem key={manager.managerId}>
