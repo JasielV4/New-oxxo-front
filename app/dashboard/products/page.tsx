e2d91bdfc99e8b442b37f56fc7877008d1087315
JasielV4 <bioplayer1234@gmail.com>
2024-11-09 18:27:45 -0700

    Estilos de busqueda

--- app/dashboard/products/page.tsx
+++ app/dashboard/products/page.tsx
@@ -1,5 +1,25 @@
-const ProductsPage = () => {
-    return null;
+import { API_URL } from "@/constants";
+import { authHeaders } from "@/helpers/authHeaders";
+import { Product } from "@/entities";
+import FilteredCards from "./_components/FilteredCard";
+
+const ProductsPage = async () => {
+    const response = await fetch(`${API_URL}/products`, {
+        headers: {
+            ...authHeaders(),
+        },
+        next: {
+            tags: ["dashboard:products"]
+        }
+    });
+    const products: Product[] = await response.json();
+    return (
+        <div className="h-[90vh] w-full">
+            <div className="w-4/12">
+                <FilteredCards products={products} />
+            </div>
+        </div>
+    )
 }
 
 export default ProductsPage;
